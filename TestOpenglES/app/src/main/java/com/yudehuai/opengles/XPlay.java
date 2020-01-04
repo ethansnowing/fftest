68e0dc19b44597c46a50dc997b554625d01c1fb8
yudehuai <yudehuai@xunlei.com>
2020-01-05 00:00:02 +0800

    修改GLSurfaceView窗口没有显示的bug

--- TestOpenglES/app/src/main/java/com/yudehuai/opengles/XPlay.java
+++ TestOpenglES/app/src/main/java/com/yudehuai/opengles/XPlay.java
@@ -2,10 +2,15 @@ package com.yudehuai.opengles;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 
-public class XPlay extends GLSurfaceView implements Runnable, SurfaceHolder.Callback {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class XPlay extends GLSurfaceView implements Runnable, SurfaceHolder.Callback,GLSurfaceView.Renderer {
+
     public XPlay(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -15,9 +20,16 @@ public class XPlay extends GLSurfaceView implements Runnable, SurfaceHolder.Call
         Open("/sdcard/out.yuv", getHolder().getSurface());
 
     }
-    @Override
-    public void surfaceCreated(SurfaceHolder var1){
+
+    public void startPlay(){
         new Thread(this).start();
+    }   //使用网上的GLSurfaceView显示方法有这个线程，但是没有调用
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            setRenderer(this);
+        }
+        new Thread(this).start();       //使用网上的GLSurfaceView显示方法没有这个开启线程，但是我这里不开启就不行
     }
 
     @Override
@@ -31,4 +43,19 @@ public class XPlay extends GLSurfaceView implements Runnable, SurfaceHolder.Call
     }
 
     public native void Open(String url, Object surface);
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
 }
